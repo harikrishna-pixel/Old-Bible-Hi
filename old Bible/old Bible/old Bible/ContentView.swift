b13ce183fdc2f1f5a2e9d4141c170485cd0adc99
harikrishna-pixel <harikrishna@marberx.com>
2025-12-19 16:06:04 +0530

    Fix nested git repo and normalize files

---
+++ old Bible/old Bible/old Bible/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  old Bible
+//
+//  Created by Barath Nagarajan on 19/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ old Bible/old Bible/old Bible/old_BibleApp.swift
@@ -0,0 +1,17 @@
+//
+//  old_BibleApp.swift
+//  old Bible
+//
+//  Created by Barath Nagarajan on 19/12/25.
+//
+
+import SwiftUI
+
+@main
+struct old_BibleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
